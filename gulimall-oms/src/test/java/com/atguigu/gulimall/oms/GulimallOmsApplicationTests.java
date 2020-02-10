0bc102ae73ced4fc688de81d74127c91c55298f0
wanjiashengfgo <493000667@qq.com>
2020-02-10 21:34:51 +0800

    创建其他模块

---
+++ gulimall-oms/src/test/java/com/atguigu/gulimall/oms/GulimallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gulimall-wms/src/test/java/com/atguigu/gulimall/wms/GulimallWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
